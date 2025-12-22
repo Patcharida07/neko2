4d31380ca3ff261c80e50bc8349d40d7738070c2
game22403035 <game22403019@gmail.com>
2025-12-22 10:35:27 +0900

    555
    5555

--- Assets/nana/ScriptsLevel2/SpotlightMover.cs
+++ Assets/nana/ScriptsLevel2/SpotlightMover.cs
@@ -1,20 +1,67 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SpotlightMover : MonoBehaviour
 {
-    public float moveAmplitude = 4f; // ระยะทางแกว่งซ้าย-ขวา
-    public float moveSpeed = 2f;     // ความเร็วการแกว่ง
+    public float moveAmplitude = 4f;   // 左右最大距离
+    public float moveSpeed = 2f;       // 移动速度
+    public float waitTime = 3f;        // 端点停顿时间（秒）
 
     private Vector3 startPos;
+    private int direction = 1;         // 1 = 向右，-1 = 向左
+    private bool isWaiting = false;
 
     void Start()
     {
         startPos = transform.position;
+        StartCoroutine(MoveRoutine());
     }
 
-    void Update()
+    IEnumerator MoveRoutine()
     {
-        float xOffset = Mathf.Sin(Time.time * moveSpeed) * moveAmplitude;
-        transform.position = new Vector3(startPos.x + xOffset, startPos.y, startPos.z);
+        while (true)
+        {
+            if (!isWaiting)
+            {
+                // 移动
+                transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+
+                float distanceFromStart = transform.position.x - startPos.x;
+
+                // 到达右端
+                if (distanceFromStart >= moveAmplitude)
+                {
+                    transform.position = new Vector3(
+                        startPos.x + moveAmplitude,
+                        startPos.y,
+                        startPos.z
+                    );
+
+                    direction = -1;
+                    yield return StartCoroutine(WaitAtEdge());
+                }
+                // 到达左端
+                else if (distanceFromStart <= -moveAmplitude)
+                {
+                    transform.position = new Vector3(
+                        startPos.x - moveAmplitude,
+                        startPos.y,
+                        startPos.z
+                    );
+
+                    direction = 1;
+                    yield return StartCoroutine(WaitAtEdge());
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator WaitAtEdge()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);
+        isWaiting = false;
     }
 }
