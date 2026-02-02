2348072bef2dd6e7a68d31251bf175f3ecf05073
Patcharida07 <171124005+Patcharida07@users.noreply.github.com>
2026-02-02 12:04:30 +0900

    level3

--- Assets/rinkinnetsu/01/Congratulation31.cs
+++ Assets/rinkinnetsu/01/Congratulation31.cs
@@ -1,22 +1,46 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Congratulation31 : MonoBehaviour
 {
+    // 🔁 Restart ด่าน 3 ใหม่หมด
+    public void ReplayGameButtonn()
+    {
+        Debug.Log("Restart Level 3");
+
+        if (GameManager.Instance != null)
+            GameManager.Instance.ResetForRestartLevel();
+
+        SceneManager.LoadScene("Level3");
+    }
+
+     //🎮 Retry Puzzle(ผู้เล่นอยู่จุดเดิม)
+    public void RetryPuzzleButton()
+    {
+        Debug.Log("Retry Puzzle");
+
+        if (GameManager.Instance != null)
+            GameManager.Instance.ResetForRetryPuzzle();
+
+        SceneManager.LoadScene("NewPuzzle");
+    }
+
+    // ➡️ ไปด่านถัดไป
     public void NextlevelGameButton()
     {
         Debug.Log("Next Level");
+
         SceneManager.LoadScene("Level4");
     }
-    public void ReplayGameButtonn()
-    {
-        Debug.Log("Replay");
-        SceneManager.LoadScene("Level3");
-    }
+
+    // 🏠 Restart ทั้งเกม (กลับ Start)
     public void returnButton()
     {
-        Debug.Log("return");
+        Debug.Log("Restart Whole Game");
+
+        if (GameManager.Instance != null)
+            GameManager.Instance.ResetForNewGame();
+
         SceneManager.LoadScene("Start");
     }
-
 }
