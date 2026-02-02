b074805af5c107635c921d9933dafe95ba1b06bd
game22403035 <game22403019@gmail.com>
2026-02-02 11:15:17 +0900

    99999
    9999

---
+++ Assets/nana/ScriptsLevel5/LeftButton5.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LeftButton5 : MonoBehaviour
+{
+    public GameObject leftDoorPart;
+    public GameObject lightA;   // ← 灯
+
+    private bool canPress;
+
+    void Update()
+    {
+        if (canPress && Input.GetKeyDown(KeyCode.E))
+        {
+            leftDoorPart.SetActive(true);
+            lightA.SetActive(true);   // ← 关键就这一行
+
+            Debug.Log("LEFT DOOR + LIGHT A ON");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Shadow"))
+            canPress = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Shadow"))
+            canPress = false;
+    }
+}
