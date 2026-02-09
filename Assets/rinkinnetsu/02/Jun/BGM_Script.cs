d0f8ee76fa476e09b5217208af3dfcb1e14d39e4
game22403035 <game22403019@gmail.com>
2026-02-09 10:33:33 +0900

    11
    11

--- Assets/rinkinnetsu/02/Jun/BGM_Script.cs
+++ Assets/rinkinnetsu/02/Jun/BGM_Script.cs
@@ -1,20 +1,35 @@
-using UnityEngine;
-using UnityEngine.Audio;
+﻿using UnityEngine;
 
 public class BGM_Script : MonoBehaviour
 {
     public static BGM_Script instance;
 
+    private AudioSource audioSource;
+
     private void Awake()
     {
         if (instance == null)
         {
-            DontDestroyOnLoad(this.gameObject);
             instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            audioSource = GetComponent<AudioSource>();
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    // 给 Slider 调用的方法
+    public void SetVolume(float value)
+    {
+        audioSource.volume = value;
+    }
+
+    // 如果你想让 Slider 读取当前音量
+    public float GetVolume()
+    {
+        return audioSource.volume;
     }
 }
